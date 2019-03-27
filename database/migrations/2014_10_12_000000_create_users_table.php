7015e2d9bd4122893b6530d59271d34e4ac21462
casalazar562 <casalazar562@misena.edu.co>
2019-03-27 11:29:55 -0500

    migration user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,16 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone_1');
+            $table->string('phone_2');
+            $table->string('picture');
+            $table->date('birthday');
+            $table->enum('gender',['Male','Female','Other']);
+            $table->string('slug');
             $table->rememberToken();
             $table->timestamps();
         });
