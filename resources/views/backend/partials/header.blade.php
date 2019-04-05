9f1212400c4b42f5c46d183a4452d2e783494c9b
Omar David Sanchez Arroyo <odsanchez46@misena.edu.co>
2019-04-05 12:55:03 -0500

    Envio correo para nuevo usuario

--- resources/views/backend/partials/header.blade.php
+++ resources/views/backend/partials/header.blade.php
@@ -7,8 +7,10 @@
             <span class="input-group-btn">
             <button class="btn btn-secondary" type="button"><i class="fas fa-search"></i></button>
           </span>
-        </div><!-- input-group -->
-    </div><!-- br-header-left -->
+        </div>
+        <!-- input-group -->
+    </div>
+    <!-- br-header-left -->
     <div class="br-header-right">
         <nav class="nav">
             <div class="dropdown">
@@ -22,7 +24,8 @@
                     <div class="dropdown-menu-label">
                         <label>Messages</label>
                         <a href="">+ Add New Message</a>
-                    </div><!-- d-flex -->
+                    </div>
+                    <!-- d-flex -->
 
                     <div class="media-list">
                         <!-- loop starts here -->
@@ -33,10 +36,13 @@
                                     <div>
                                         <p>Donna Seay</p>
                                         <span>2 minutes ago</span>
-                                    </div><!-- d-flex -->
-                                    <p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring.</p>
+                                    </div>
+                                    <!-- d-flex -->
+                                    <p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings
+                                        of spring.</p>
                                 </div>
-                            </div><!-- media -->
+                            </div>
+                            <!-- media -->
                         </a>
                         <!-- loop ends here -->
                         <a href="" class="media-list-link read">
@@ -46,10 +52,12 @@
                                     <div>
                                         <p>Samantha Francis</p>
                                         <span>3 hours ago</span>
-                                    </div><!-- d-flex -->
+                                    </div>
+                                    <!-- d-flex -->
                                     <p>My entire soul, like these sweet mornings of spring.</p>
                                 </div>
-                            </div><!-- media -->
+                            </div>
+                            <!-- media -->
                         </a>
                         <a href="" class="media-list-link read">
                             <div class="media">
@@ -58,10 +66,12 @@
                                     <div>
                                         <p>Robert Walker</p>
                                         <span>5 hours ago</span>
-                                    </div><!-- d-flex -->
+                                    </div>
+                                    <!-- d-flex -->
                                     <p>I should be incapable of drawing a single stroke at the present moment...</p>
                                 </div>
-                            </div><!-- media -->
+                            </div>
+                            <!-- media -->
                         </a>
                         <a href="" class="media-list-link read">
                             <div class="media">
@@ -70,17 +80,22 @@
                                     <div>
                                         <p>Larry Smith</p>
                                         <span>Yesterday</span>
-                                    </div><!-- d-flex -->
+                                    </div>
+                                    <!-- d-flex -->
                                     <p>When, while the lovely valley teems with vapour around me, and the meridian sun strikes...</p>
                                 </div>
-                            </div><!-- media -->
+                            </div>
+                            <!-- media -->
                         </a>
                         <div class="dropdown-footer">
                             <a href=""><i class="fas fa-angle-down"></i> Show All Messages</a>
                         </div>
-                    </div><!-- media-list -->
-                </div><!-- dropdown-menu -->
-            </div><!-- dropdown -->
+                    </div>
+                    <!-- media-list -->
+                </div>
+                <!-- dropdown-menu -->
+            </div>
+            <!-- dropdown -->
             <div class="dropdown">
                 <a href="" class="nav-link pd-x-7 pos-relative" data-toggle="dropdown">
                     <i class="icon ion-ios-bell-outline tx-24"></i>
@@ -92,7 +107,8 @@
                     <div class="dropdown-menu-label">
                         <label>Notifications</label>
                         <a href="">Mark All as Read</a>
-                    </div><!-- d-flex -->
+                    </div>
+                    <!-- d-flex -->
 
                     <div class="media-list">
                         <!-- loop starts here -->
@@ -103,7 +119,8 @@
                                     <p class="noti-text"><strong>Suzzeth Bungaos</strong> tagged you and 18 others in a post.</p>
                                     <span>October 03, 2017 8:45am</span>
                                 </div>
-                            </div><!-- media -->
+                            </div>
+                            <!-- media -->
                         </a>
                         <!-- loop ends here -->
                         <a href="" class="media-list-link read">
@@ -113,7 +130,8 @@
                                     <p class="noti-text"><strong>Mellisa Brown</strong> appreciated your work <strong>The Social Network</strong></p>
                                     <span>October 02, 2017 12:44am</span>
                                 </div>
-                            </div><!-- media -->
+                            </div>
+                            <!-- media -->
                         </a>
                         <a href="" class="media-list-link read">
                             <div class="media">
@@ -122,7 +140,8 @@
                                     <p class="noti-text">20+ new items added are for sale in your <strong>Sale Group</strong></p>
                                     <span>October 01, 2017 10:20pm</span>
                                 </div>
-                            </div><!-- media -->
+                            </div>
+                            <!-- media -->
                         </a>
                         <a href="" class="media-list-link read">
                             <div class="media">
@@ -131,14 +150,18 @@
                                     <p class="noti-text"><strong>Julius Erving</strong> wants to connect with you on your conversation with <strong>Ronnie Mara</strong></p>
                                     <span>October 01, 2017 6:08pm</span>
                                 </div>
-                            </div><!-- media -->
+                            </div>
+                            <!-- media -->
                         </a>
                         <div class="dropdown-footer">
                             <a href=""><i class="fas fa-angle-down"></i> Show All Notifications</a>
                         </div>
-                    </div><!-- media-list -->
-                </div><!-- dropdown-menu -->
-            </div><!-- dropdown -->
+                    </div>
+                    <!-- media-list -->
+                </div>
+                <!-- dropdown-menu -->
+            </div>
+            <!-- dropdown -->
             <div class="dropdown">
                 <a href="" class="nav-link nav-link-profile" data-toggle="dropdown">
                     <span class="logged-name hidden-md-down">Katherine</span>
@@ -164,10 +187,20 @@
                         <li><a href=""><i class="icon ion-ios-download"></i> Downloads</a></li>
                         <li><a href=""><i class="icon ion-ios-star"></i> Favorites</a></li>
                         <li><a href=""><i class="icon ion-ios-folder"></i> Collections</a></li>
-                        <li><a href=""><i class="icon ion-power"></i> Sign Out</a></li>
+                        <li>
+                            <a href="{{ route('logout') }}" onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();">
+                                <i class="icon ion-power"></i> {{ __('Logout') }}
+                             </a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        </li>
                     </ul>
-                </div><!-- dropdown-menu -->
-            </div><!-- dropdown -->
+                </div>
+                <!-- dropdown-menu -->
+            </div>
+            <!-- dropdown -->
         </nav>
         <div class="navicon-right">
             <a id="btnRightMenu" href="" class="pos-relative">
@@ -176,6 +209,8 @@
                 <span class="square-8 bg-danger pos-absolute t-10 r--5 rounded-circle"></span>
                 <!-- end: if statement -->
             </a>
-        </div><!-- navicon-right -->
-    </div><!-- br-header-right -->
+        </div>
+        <!-- navicon-right -->
+    </div>
+    <!-- br-header-right -->
 </div>
